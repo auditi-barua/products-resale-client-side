06412b13078b2390865aa7fa1322fe6e38f13d81
Auditi <auditibarua6@gmail.com>
2022-11-24 14:38:34 +0300

    added navbar,banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen " style={{ backgroundImage: `url("https://i.ibb.co/YhDxvFs/bedroom.jpg")` }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl  text-gray-900  font-bold">Furnitureland</h1>
+            <p className="mb-5 text-gray-300">Explore our furniture & home furnishing range. Our Furniture is a bangladeshi leader in life at home.First and Largest Furniture Online shop in Bangladesh. Upto 50% Off on Furniture Price in BD. </p>
+            <button className="btn btn-primary">Shop Now</button>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { FaCouch } from "react-icons/fa";
 const Navbar = () => {
     const items = <React.Fragment>
-      <li><Link to='/home'>Home</Link></li>
+      <li><Link to='/home' className='text-blue-600'>Home</Link></li>
       <li tabIndex={0}>
         <Link to='/products'> Products</Link></li>
         <li><Link to='/shop'>Shop</Link></li>
@@ -18,14 +18,14 @@ const Navbar = () => {
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 text-green-600 rounded-box w-52">
                         {items}
       </ul>
     </div>
-    <Link to='/' className="text-green-600 normal-case font-bold text-xl">Furnitureland</Link>
+    <Link to='/' className="text-green-600  flex normal-case font-bold text-2xl"><FaCouch className='w-10'/>Furnitureland</Link>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal p-0">
+    <ul className="menu text-green-600 text-xl menu-horizontal p-0">
     {items}
     </ul>
   </div>
