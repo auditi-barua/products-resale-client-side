509e8bdb8a3c8e7d872644e453c6c0a1c7b3dc7e
Auditi <auditibarua6@gmail.com>
2022-11-26 20:57:08 +0300

    added category

--- src/Pages/Category/Categories.js
+++ src/Pages/Category/Categories.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Categories = ({category}) => {
     const { category_name, img } = category;
     return (
-        <div className='card mt-5 bg-gray-600  shadow-xl'>
-            <figure className="px-5 pt-5">
-                <img src={img} alt='' className='w-24' />
-       </figure>
-            <div className="card-body items-center text-center">
-                <h2 className="card-title text-white">{category_name}</h2>    
-            </div>
-            <button className='btn btn-primary'>See details</button>
-   </div>
+        <div className="card mt-5 card-compact w-86 bg-gray-600 shadow-xl">
+        <figure><img src={img} className='w-96 h-48 ' alt="Shoes" /></figure>
+        <div className="card-body">
+                <h2 className="card-title text-white">Category: {category_name}</h2>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     );
 };
 
